e761407388cf738e52b51a303f6053bf4278ba74
estevan.teixeira <estevan.teixeira@mprj.mp.br>
2018-09-03 19:04:47 -0300

    Início da recuperação de temperatura

---
+++ clima.py
@@ -0,0 +1,20 @@
+import requests
+from decouple import config
+
+URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations/SBRJ.TXT"
+
+proxyDict = {
+    "http": config('HTTP_PROXY'),
+    "https": config('HTTP_PROXY')
+}
+
+
+def read():
+    print(proxyDict)
+    contents = requests.get(URL, proxies=proxyDict).content
+
+    print(contents)
+    json = {
+        "temperature": "NULL"
+    }
+    return json
--- orgaos.py
+++ orgaos.py
@@ -1,8 +1,29 @@
 from database import DAO
 
+list_orgaos_query = f"""
+    SELECT 
+        org.ORLW_ORGI_CDORGAO AS CDORG,
+        org.ORLW_REGI_NM_REGIAO AS CRAAI,
+        org.ORLW_CMRC_NM_COMARCA AS COMARCA,
+        org.ORLW_COFO_NM_FORO AS FORO,
+        org.ORLW_ORGI_NM_ORGAO AS ORGAO,
+        func.NMFUNCIONARIO AS TITULAR
+    FROM 
+        ORGI_VW_ORGAO_LOCAL_ATUAL org
+        LEFT JOIN MPRJ_VW_FUNCIONARIO func ON func.CDORGAO = org.ORLW_DK
+    WHERE
+        1=1
+        AND org.ORLW_TPOR_DK = 1
+        AND (org.ORLW_ORGI_DT_FIM IS NULL
+            OR org.ORLW_ORGI_DT_FIM > SYSDATE)
+        AND func.CDTIPFUNC = 1 
+        AND func.CDSITUACAOFUNC = 1
+        AND func.CDCARGO = 74
+"""
 
-def read():
-    data = DAO.run()
+
+def list_orgaos():
+    data = DAO.run(list_orgaos_query)
     results = []
     for row in data:
         row_dict = {
