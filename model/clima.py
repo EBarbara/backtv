46d63c879c47ce9b575424c7d566b661e18560a5
Daniel Belchior <danielbelchior@gmail.com>
2018-09-04 14:40:16 -0300

    clima e news funcionando

--- model/clima.py
+++ model/clima.py
@@ -1,6 +1,10 @@
 import requests
 from decouple import config
 
+from metar import Metar
+import urllib.request
+
+
 URL = "http://tgftp.nws.noaa.gov/data/observations/metar/stations/SBRJ.TXT"
 
 proxyDict = {
@@ -8,13 +12,10 @@ proxyDict = {
     "https": config('HTTP_PROXY')
 }
 
-
 def read():
-    print(proxyDict)
-    contents = requests.get(URL, proxies=proxyDict).content
-
-    print(contents)
+    contents = requests.get(URL, proxies=proxyDict).content.decode('UTF-8')
+    obs = Metar.Metar('METAR ' + contents.split('\n')[1])
     json = {
-        "temperature": "NULL"
+        "temperature": str(obs.temp).split(' ')[0]
     }
     return json
---
+++ model/news.py
@@ -0,0 +1,31 @@
+import requests
+from decouple import config
+
+import feedparser
+import json
+from operator import itemgetter
+
+proxyDict = {
+    "http": config('HTTP_PROXY'),
+    "https": config('HTTP_PROXY')
+}
+
+def read():
+    ebc = feedparser.parse('http://agenciabrasil.ebc.com.br/rss/ultimasnoticias/feed.xml')
+    valor_politica = feedparser.parse('https://www.valor.com.br/politica/rss')
+    valor_brasil = feedparser.parse('https://www.valor.com.br/brasil/rss')
+
+    arr = []
+
+    for entry in valor_politica.entries:
+        arr.append({'source':'Valor Econômico - Política', 'title': entry.title, 'summary':entry.summary, 'href': entry.links[0].href, 'published': entry.published, 'published_parsed': entry.published_parsed})    
+
+    for entry in valor_brasil.entries:
+        arr.append({'source':'Valor Econômico - Brasil', 'title': entry.title, 'summary':entry.summary, 'href': entry.links[0].href, 'published': entry.published, 'published_parsed': entry.published_parsed})    
+
+    for entry in ebc.entries:
+        arr.append({'source':'Empresa Brasil de Comunicação', 'title': entry.title, 'summary':entry.summary[0:200], 'href': entry.links[0].href, 'published': entry.published, 'published_parsed': entry.published_parsed})    
+
+    sorted(arr,key=itemgetter("published_parsed"))
+
+    return json.dumps(arr)
